1a60d1683a2b613baeefad06685df78f9041cc42
tzulunyeh <you@examplegit config --global user.name Your>
2021-05-28 15:26:39 +0800

    10 commit

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,9 +146,7 @@ LOGGING = {
     },
 }
 
-'whitenoise.middleware.WhiteNoiseMiddleware',
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
